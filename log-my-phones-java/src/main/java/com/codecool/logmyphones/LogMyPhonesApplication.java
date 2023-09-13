ac0c208e88d324d0ee66164d5c1af894fae14a9f
somavrg <varga.j.soma@gmail.com>
2023-09-13 12:01:20 +0200

    feat: create application class

---
+++ log-my-phones-java/src/main/java/com/codecool/logmyphones/LogMyPhonesApplication.java
@@ -0,0 +1,12 @@
+package com.codecool.logmyphones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogMyPhonesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogMyPhonesApplication.class, args);
+    }
+}
