4c282ea22b8b538a85977b2b27e6ec73df5b7b2a
somavrg <varga.j.soma@gmail.com>
2023-09-13 17:44:00 +0200

    feat: implement CallController methods

--- backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/controller/CallController.java
+++ backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/controller/CallController.java
@@ -1,4 +1,30 @@
 package com.codecool.logmyphones.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("calls")
 public class CallController {
+    private final List<String> calls = new ArrayList<>();
+    @GetMapping("/")
+    public List<String> getAllCalls() {
+
+        //TODO: callService.getCalls()
+
+        return new ArrayList<>(calls);
+    }
+
+    @GetMapping("/{id}")
+    public String getCallById(@PathVariable int id) {
+
+        //TODO: callService.getCallById(id)
+
+        return calls.get(id);
+    }
 }
