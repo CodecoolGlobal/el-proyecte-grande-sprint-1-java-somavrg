84246cfdaa2f18e9cf91cf4132573b7203cf1d45
Kapus Tamás <kapustamas@gmail.com>
2023-09-14 12:17:54 +0200

    feat: create phone endpoints

--- backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/controller/PhoneController.java
+++ backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/controller/PhoneController.java
@@ -1,4 +1,47 @@
+
 package com.codecool.logmyphones.controller;
 
+import com.codecool.logmyphones.modell.Phone;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/phones")
 public class PhoneController {
+    private final List<Phone> phones = new ArrayList<>();
+
+    @GetMapping("/{companyId}")
+    public List<Phone> getPhones(@PathVariable int companyId) {
+        //TODO: phoneService.getPhonesByCompanyId(companyId)
+        return new ArrayList<>(phones);
+    }
+
+    @GetMapping("/{id}")
+    public Phone getPhoneById(@PathVariable int id) {
+        // TODO: phoneService.getPhoneById(id)
+        return phones.get(id);
+    }
+
+    @PostMapping("")
+    public boolean addNewPhone(@RequestBody Phone phone) {
+        // TODO: phoneService.addNewPhone(phone)
+        return false;
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Phone> patchPhone(@PathVariable int id, @RequestBody Phone phoneDetails) {
+        // TODO: phoneService.patchPhone(id, phoneDetails)
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean deletePhone(@PathVariable int id) {
+        // TODO: phoneService.deletePhoneById(id)
+        return false;
+    }
 }
+
