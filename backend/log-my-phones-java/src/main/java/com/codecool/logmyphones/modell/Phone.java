62bd2c400f7ed2e4d62512ad8a3a3d4e536da48e
somavrg <varga.j.soma@gmail.com>
2023-09-26 13:25:33 +0200

    fix: implement Phone entity class

--- backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/modell/Phone.java
+++ backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/modell/Phone.java
@@ -1,4 +1,18 @@
 package com.codecool.logmyphones.modell;
 
-public record Phone(int phoneId, String name, String phoneNumber) {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Phone {
+    @Id
+    private int phoneNumber;
 }
