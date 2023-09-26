fdb62cb215407e07d320217f5c74ef167e8d2180
somavrg <varga.j.soma@gmail.com>
2023-09-26 16:18:32 +0200

    feat: create correct DispatcherController with service layer's methods commented

---
+++ backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/controller/DispatcherController.java
@@ -0,0 +1,59 @@
+
+package com.codecool.logmyphones.controller;
+
+import com.codecool.logmyphones.model.Dispatcher;
+import com.codecool.logmyphones.model.Phone;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/phones")
+public class DispatcherController {
+
+    //TODO service
+
+    //    private final DispatcherService dispatcherService;
+    //
+    //    @Autowired
+    //    public DispatcherController(DispatcherService dispatcherService) {
+    //        this.dispatcherService = dispatcherService;
+    //    }
+
+
+    @GetMapping("/{companyId}")
+    public Set<Dispatcher> getAllDispatchersByCompany(@PathVariable int companyId) {
+        //TODO: dispatcherService.getDispatchersByCompanyId(companyId)
+        return null;
+    }
+
+    @GetMapping("/{companyId}/{id}")
+    public Dispatcher getDispatcherById(@PathVariable int companyId, @PathVariable int id) {
+        // TODO: dispatcherService.getDispatcherById(id)
+        return null;
+    }
+
+    @PostMapping("/{companyId}")
+    public boolean addNewDispatcher(@PathVariable int companyId, @RequestBody Dispatcher dispatcher) {
+        // TODO: dispatcherService.addNewDispatcher(dispatcher)
+        return false;
+    }
+
+    @PatchMapping("/{companyId}/{id}")
+    public void updateDispatcherById(@PathVariable int companyId,
+                                     @PathVariable int id,
+                                     @RequestBody Dispatcher dispatcherUpdate) {
+        // TODO: dispatcherService.updateDispatcher(id, dispatcherUpdate)
+    }
+
+    @DeleteMapping("/{companyId}/{id}")
+    public boolean deleteDispatcher(@PathVariable int companyId, @PathVariable int id) {
+        // TODO: dispatcherService.deleteDispatcherById(id)
+        return false;
+    }
+}
+
