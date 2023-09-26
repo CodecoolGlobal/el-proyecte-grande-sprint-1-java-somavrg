c71c7e045c3b1087c87e10bc7902543e13701cc7
Kapus Tamás <kapustamas@gmail.com>
2023-09-26 14:28:41 +0200

    fix: fix PhoneDAO

--- backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/model/DAO/PhoneDAO.java
+++ backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/model/DAO/PhoneDAO.java
@@ -3,5 +3,5 @@ package com.codecool.logmyphones.model.DAO;
 import com.codecool.logmyphones.model.Phone;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PhoneDAO extends JpaRepository<Phone,Long> {
+public interface PhoneDAO extends JpaRepository<Phone,String> {
 }
