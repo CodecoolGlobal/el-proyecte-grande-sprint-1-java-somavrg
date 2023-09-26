fac53711d6a277686b4aaca5003351417a1d85fa
somavrg <varga.j.soma@gmail.com>
2023-09-26 13:22:13 +0200

    fix: implement Contact entity class

--- backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/modell/Contact.java
+++ backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/modell/Contact.java
@@ -1,4 +1,25 @@
 package com.codecool.logmyphones.modell;
 
-public record Contact(int contactId, String name, String phoneNumber) {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int contactId;
+    private String name;
+    @OneToOne(mappedBy = "phone")
+    private Phone phone;
+    private String info;
+    @Transient
+    @ManyToOne
+    private User user;
 }
