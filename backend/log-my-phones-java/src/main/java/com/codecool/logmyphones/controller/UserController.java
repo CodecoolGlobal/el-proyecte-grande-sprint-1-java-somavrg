bb5e1b8eef4a83521138202386d52632dc67882e
somavrg <varga.j.soma@gmail.com>
2023-09-26 16:23:59 +0200

    feat: create correct UserController with service layer's methods commented

--- backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/controller/UserController.java
+++ backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/controller/UserController.java
@@ -11,57 +11,43 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    private final List<User> users = new ArrayList<>(List.of(
-            new User(1, "Jamie", "jamie@email.com"),
-            new User(2, "Johnathan", "johnathan@email.com"),
-            new User(3, "Marie", "marie@email.com"),
-            new User(4, "Daniel", "daniel@email.com"),
-            new User(5, "Samantha", "samantha@email.com"),
-            new User(6, "Michaela", "michaela@email.com"),
-            new User(7, "Emilia", "emilia@email.com"),
-            new User(8, "William", "william@email.com"),
-            new User(9, "Oliver", "oliver@email.com"),
-            new User(10, "Danielle", "danielle@email.com"),
-            new User(11, "Sophie", "sophie@email.com"),
-            new User(12, "Matthew", "matthew@email.com"),
-            new User(13, "Ava", "ava@email.com"),
-            new User(14, "Chris", "chris@email.com"),
-            new User(15, "Emma", "emma@email.com"),
-            new User(16, "Josephine", "josephine@email.com"),
-            new User(17, "Charlie", "charlie@email.com"),
-            new User(18, "Benjamin", "benjamin@email.com"),
-            new User(19, "Mia", "mia@email.com"),
-            new User(20, "Alex", "alex@email.com"),
-            new User(21, "Grace", "grace@email.com")
-    ));
+
+    //TODO service
+
+    //    private final UserService userService;
+    //
+    //    @Autowired
+    //    public UserController(UserService userService) {
+    //        this.userService = userService;
+    //    }
 
 
     @GetMapping("")
-    public List<User> getUsers() {
-        return new ArrayList<>(users);
+    public List<User> getAllUsers() {
+        //TODO: userService.getAllUsers()
+        return null;
     }
 
     @GetMapping("/{id}")
     public User getUserById(@PathVariable int id) {
         //TODO: userService.getUserById(id)
-        return users.get(id);
+        return null;
     }
 
     @PostMapping("")
     public boolean addNewUser(@RequestBody User user) {
         //TODO: userService.addNewUser(user)
-        return users.add(user);
+        return false;
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<User> patchUser(@PathVariable int id, @RequestBody User userDetails) {
-        //TODO: userService.patchUser(id,userDetails)
-        return new  ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    public void updateUser(@PathVariable int id, @RequestBody User userUpdate) {
+        //TODO: userService.updateUser(id, userUpdate)
     }
 
     @DeleteMapping("/{id}")
     public boolean deleteUser(@PathVariable int id) {
         //TODO: userService.deleteUserById(id)
-        return users.remove(users.get(id));
+        return false;
     }
 }
