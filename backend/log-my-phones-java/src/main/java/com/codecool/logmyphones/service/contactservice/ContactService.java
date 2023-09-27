15353440b146f988284fe80f38986d90247b773d
somavrg <varga.j.soma@gmail.com>
2023-09-27 17:55:15 +0200

    feat: implement ContactService

--- backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/service/contactservice/ContactService.java
+++ backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/service/contactservice/ContactService.java
@@ -1,4 +1,13 @@
 package com.codecool.logmyphones.service.contactservice;
 
+import com.codecool.logmyphones.model.DTO.ContactDTO;
+
+import java.util.Set;
+
 public interface ContactService {
+    Set<ContactDTO> getAllContacts();
+    ContactDTO getContactById(Long id);
+    void addNewContact(ContactDTO contactDTO);
+    void updateContact(Long id, ContactDTO contactDTO);
+    void deleteContact(Long id);
 }
