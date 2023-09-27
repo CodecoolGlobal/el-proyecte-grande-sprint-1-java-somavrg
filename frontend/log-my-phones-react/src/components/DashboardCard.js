5acd0442f9e860d548044fde6fdeedd064cb7cf0
oliver-nagy <olivernagy490@gmail.com>
2023-09-27 17:42:19 +0200

    feat: implement dashboard card component

---
+++ frontend/log-my-phones-react/src/components/DashboardCard.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import {Button, Card, CardActions, CardContent, createTheme, ThemeProvider} from "@mui/material";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+
+const theme = createTheme({
+    typography: {
+        button: {
+            textTransform: "none",
+            fontWeight: 'bold',
+        }
+    }
+});
+
+function DashboardCard({ data }) {
+    console.log(theme.palette[`${data.infoType}`].main)
+    console.log(theme.palette[`${data.infoType}`].main + '00')
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Card elevation={2} sx={{
+                borderRadius: 4,
+                background: `linear-gradient(to bottom right,
+                    ${theme.palette[`${data.infoType}`].main + '44'} 0%,
+                    ${theme.palette[`${data.infoType}`].main + '00'} 80%)`
+            }}>
+                <CardContent
+                    spacing={3}
+                    sx={{
+                        mx: 5,
+                        my: 2,
+                    display: 'flex',
+                        gap: '24px',
+                    justifyContent: 'flex-start',
+                    alignItems: 'center'
+}}>
+
+                        {data.icon}
+
+                    <Box sx={{
+                        display: 'inline'
+                    }}>
+                        <Box sx={{
+                            display:'inline-block'
+                        }}>
+                            <Box sx={{
+                                fontWeight: 'light',
+                                color: 'text.secondary',
+                            }}>{data.title}</Box>
+                            <Box sx={{my:0.5}}>
+                                <Typography variant={"h4"} sx={{
+                                    color: 'text.primary',
+                                    fontWeight: 'bold'
+
+                                }}>
+                                    {data.statistic}
+                                </Typography>
+                            </Box>
+                        </Box>
+                    </Box>
+                </CardContent>
+                <Divider />
+                <CardActions>
+                    <Button size="small" sx={{
+                        color: 'text.primary',
+                        '&:hover': {
+                            backgroundColor: '#ffffff00',
+                            color: 'text.primary',
+                        }
+                    }}>{data.buttonText}
+                        <ArrowForwardIcon sx={{fontSize: 'medium', m: 0.5}}/>
+                    </Button>
+                </CardActions>
+            </Card>
+        </ThemeProvider>
+    );
+}
+
+export default DashboardCard;
--- frontend/log-my-phones-react/src/pages/Dashboard.js
+++ frontend/log-my-phones-react/src/pages/Dashboard.js
@@ -1,17 +1,48 @@
-import {Card, Grid} from "@mui/material";
+import {Card, Grid, makeStyles} from "@mui/material";
+import DashboardCard from "../components/DashboardCard";
+import PhoneInTalkTwoToneIcon from '@mui/icons-material/PhoneInTalkTwoTone';
+import WatchLaterTwoToneIcon from '@mui/icons-material/WatchLaterTwoTone';
+import PhoneMissedTwoToneIcon from '@mui/icons-material/PhoneMissedTwoTone';
+
+const DASHBOARD_DATA_ICON_SIZE = 40;
 
 const Dashboard = () => {
+    const dashboardData = [
+        {
+            icon: <PhoneInTalkTwoToneIcon sx={{fontSize: DASHBOARD_DATA_ICON_SIZE}} />,
+            title: 'Handled calls',
+            statistic: 624,
+            buttonText: 'See calls',
+            infoType: 'success'
+        },
+        {
+            icon: <WatchLaterTwoToneIcon sx={{fontSize: DASHBOARD_DATA_ICON_SIZE}}/>,
+            title: 'Avg. call duration',
+            statistic: 367,
+            buttonText: 'See statistics',
+            infoType: 'info'
+        },
+        {
+            icon: <PhoneMissedTwoToneIcon sx={{fontSize: DASHBOARD_DATA_ICON_SIZE}}/>,
+            title: 'Missed calls',
+            statistic: 4,
+            buttonText: 'See pending calls',
+            infoType: 'error'
+        },
+
+    ]
     return (
         <div>
-            <Grid container>
-                <Grid item>
-
-                </Grid>
-                <Grid item></Grid>
-                <Grid item></Grid>
+            <Grid container spacing={4}>
+                {dashboardData.map(data => (
+                    <Grid item md={4} xs={12}>
+                        <DashboardCard data={data}/>
+                    </Grid>
+                ))}
             </Grid>
         </div>
     )
 }
 
 export default Dashboard;
+
