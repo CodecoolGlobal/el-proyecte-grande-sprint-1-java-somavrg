42436d3cb53737c623f8dd0ca3bcedd315c3a88d
Kapus Tamás <kapustamas@gmail.com>
2023-09-27 14:25:55 +0200

    feat:add custom methods to repository classes

--- backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/model/repository/CallRepository.java
+++ backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/model/repository/CallRepository.java
@@ -1,7 +1,13 @@
 package com.codecool.logmyphones.model.repository;
 
 import com.codecool.logmyphones.model.Call;
+import com.codecool.logmyphones.model.CallStatus;
+import com.codecool.logmyphones.model.Dispatcher;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Set;
+
 public interface CallRepository extends JpaRepository<Call,Long> {
+    Set<Call> getCallsByDispatcherDispatcherId(Long dispatcherId);
+    Set<Call> getCallsByDispatcherDispatcherIdAndCallStatus(Long dispatcherId,CallStatus callStatus);
 }
