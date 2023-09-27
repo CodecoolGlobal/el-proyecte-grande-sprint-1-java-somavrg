05e91a22dc427fab534172db4b0b1b545dd9bc81
Kapus Tamás <kapustamas@gmail.com>
2023-09-27 12:39:46 +0200

    fix:rename DAOs to 'Repository'

--- backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/model/DAO/UserRepository.java
+++ backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/model/DAO/UserRepository.java
@@ -3,5 +3,5 @@ package com.codecool.logmyphones.model.DAO;
 import com.codecool.logmyphones.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserDAO extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User,Long> {
 }
