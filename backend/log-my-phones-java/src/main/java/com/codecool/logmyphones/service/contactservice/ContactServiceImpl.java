b58e8c00c162dd25abc00faaa29ecdb41b1a4d87
somavrg <varga.j.soma@gmail.com>
2023-09-28 11:21:51 +0200

    feat: fix ContactServiceImpl with ContactMapper

--- backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/service/contactservice/ContactServiceImpl.java
+++ backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/service/contactservice/ContactServiceImpl.java
@@ -2,6 +2,7 @@ package com.codecool.logmyphones.service.contactservice;
 
 import com.codecool.logmyphones.model.DTO.ContactDTO;
 import com.codecool.logmyphones.model.repository.ContactRepository;
+import com.codecool.logmyphones.service.mapper.ContactMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,26 +12,28 @@ import java.util.stream.Collectors;
 @Service
 public class ContactServiceImpl implements ContactService {
     private final ContactRepository contactRepository;
+    private final ContactMapper contactMapper;
 
     @Autowired
-    public ContactServiceImpl(ContactRepository contactRepository) {
+    public ContactServiceImpl(ContactRepository contactRepository, ContactMapper contactMapper) {
         this.contactRepository = contactRepository;
+        this.contactMapper = contactMapper;
     }
 
 
     @Override
     public Set<ContactDTO> getAllContacts() {
-        return contactRepository.findAll().stream().collect(Collectors.toSet());
+        return contactMapper.toContactDTOs(contactRepository.findAll().stream().collect(Collectors.toSet()));
     }
 
     @Override
     public ContactDTO getContactById(Long id) {
-        return contactRepository.getById(id);
+        return contactMapper.toContactDTO(contactRepository.getById(id));
     }
 
     @Override
     public void addNewContact(ContactDTO contactDTO) {
-        contactRepository.save(contactDTO);
+        contactRepository.save(contactMapper.toContact(contactDTO));
     }
 
     @Override
