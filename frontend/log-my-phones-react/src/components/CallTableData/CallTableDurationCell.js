4c8f42222c35ef8f6ec9367ff9be07640335d6c2
oliver-nagy <olivernagy490@gmail.com>
2023-09-28 15:36:08 +0200

    feat: implement call table duration cell component

---
+++ frontend/log-my-phones-react/src/components/CallTableData/CallTableDurationCell.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Box from "@mui/material/Box";
+import {Grid, Typography} from "@mui/material";
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+
+function CallTableDurationCell({duration}) {
+    return (
+        <Grid container alignItems='center'>
+            <AccessTimeIcon sx={{display: 'inline-block', marginRight: '8px'}}/>
+            <Typography sx={{display: 'inline-block'}}>{duration}</Typography>
+        </Grid>
+    );
+}
+
+export default CallTableDurationCell;
