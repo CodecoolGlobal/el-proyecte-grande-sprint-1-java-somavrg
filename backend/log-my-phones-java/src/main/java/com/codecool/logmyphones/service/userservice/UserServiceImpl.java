8ba4aa05b271b7ad31286bb37eef45ba133ab10f
somavrg <varga.j.soma@gmail.com>
2023-09-28 11:19:52 +0200

    feat: fix UserServiceImpl with UserMapper

--- backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/service/userservice/UserServiceImpl.java
+++ backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/service/userservice/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.codecool.logmyphones.service.userservice;
 import com.codecool.logmyphones.model.CompanyUser;
 import com.codecool.logmyphones.model.DTO.UserDTO;
 import com.codecool.logmyphones.model.repository.UserRepository;
+import com.codecool.logmyphones.service.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,25 +13,27 @@ import java.util.stream.Collectors;
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
         this.userRepository = userRepository;
+        this.userMapper = userMapper;
     }
 
     @Override
     public Set<UserDTO> getAllUsers() {
-        return userRepository.findAll().stream().collect(Collectors.toSet());
+        return userMapper.toUserDTOs(userRepository.findAll().stream().collect(Collectors.toSet()));
     }
 
     @Override
     public UserDTO getUserById(Long id) {
-        return userRepository.getById(id);
+        return userMapper.toUserDTO(userRepository.getById(id));
     }
 
     @Override
-    public void addNewUser(UserDTO userDTO) {
-        userRepository.save(userDTO);
+    public void addNewUser(UserDTO newUserDTO) {
+        userRepository.save(userMapper.toCompanyUser(newUserDTO));
     }
 
     @Override
