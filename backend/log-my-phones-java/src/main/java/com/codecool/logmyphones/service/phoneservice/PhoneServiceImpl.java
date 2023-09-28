01d95f88414dba8bc05074ae70be519c5516e6d8
somavrg <varga.j.soma@gmail.com>
2023-09-28 11:28:04 +0200

    feat: fix PhoneServiceImpl with PhoneMapper

--- backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/service/phoneservice/PhoneServiceImpl.java
+++ backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/service/phoneservice/PhoneServiceImpl.java
@@ -3,20 +3,22 @@ package com.codecool.logmyphones.service.phoneservice;
 import com.codecool.logmyphones.model.DTO.PhoneDTO;
 import com.codecool.logmyphones.model.Phone;
 import com.codecool.logmyphones.model.repository.PhoneRepository;
+import com.codecool.logmyphones.service.mapper.PhoneMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PhoneServiceImpl implements PhoneService {
     private final PhoneRepository phoneRepository;
+    private final PhoneMapper phoneMapper;
     @Autowired
-    public PhoneServiceImpl(PhoneRepository phoneRepository) {
+    public PhoneServiceImpl(PhoneRepository phoneRepository, PhoneMapper phoneMapper) {
         this.phoneRepository = phoneRepository;
+        this.phoneMapper = phoneMapper;
     }
 
     @Override
     public void addNewPhone(PhoneDTO phoneDTO) {
-        Phone phone = Phone.builder().phoneNumber(phoneDTO.phoneNumber()).build();
-            phoneRepository.save(phone);
+        phoneRepository.save(phoneMapper.toPhone(phoneDTO));
     }
 }
