f888ae2d24c44f438a5bdad69234ef415d23f123
Kapus Tamás <kapustamas@gmail.com>
2023-09-28 10:34:57 +0200

    fix: change Dispatcher to DiscpatcherDTO in CallDTO

--- backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/model/DTO/CallDTO.java
+++ backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/model/DTO/CallDTO.java
@@ -2,13 +2,12 @@ package com.codecool.logmyphones.model.DTO;
 
 import com.codecool.logmyphones.model.CallDirection;
 import com.codecool.logmyphones.model.CallStatus;
-import com.codecool.logmyphones.model.Dispatcher;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
 @Builder
 public record CallDTO(
-        Dispatcher dispatcher,
+        DispatcherDTO dispatcher,
         PhoneDTO client,
         LocalDateTime startTime,
         CallStatus callStatus,
