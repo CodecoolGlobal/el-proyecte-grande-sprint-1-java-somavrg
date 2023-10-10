986bb10f3828cd3b1e1f3d88e8995815a7f6f0dc
somavrg <varga.j.soma@gmail.com>
2023-10-10 15:31:10 +0200

    feat: create Application Config

---
+++ backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/security/ApplicationConfig.java
@@ -0,0 +1,22 @@
+package com.codecool.logmyphones.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
