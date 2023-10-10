9a5c01136863ba12349f809ae28f8cf586029bb0
somavrg <varga.j.soma@gmail.com>
2023-10-10 15:38:08 +0200

    feat: create Security Config class

---
+++ backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.codecool.logmyphones.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/auth/**")
+                .permitAll();
+
+        return http.build();
+    }
+}
