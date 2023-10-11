bc8400dbbbaef501d7c21c5b0b08bbd23b05173d
somavrg <varga.j.soma@gmail.com>
2023-10-11 16:47:35 +0200

    feat: create getUserByEmail

--- backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/model/repository/UserRepository.java
+++ backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/model/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.codecool.logmyphones.model.repository;
 import com.codecool.logmyphones.model.CompanyUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<CompanyUser,Long> {
-    public CompanyUser getCompanyUserByEmail(String email);
+    Optional<CompanyUser> findByEmail(String email);
 }
--- backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/service/userservice/UserService.java
+++ backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/service/userservice/UserService.java
@@ -1,7 +1,9 @@
 package com.codecool.logmyphones.service.userservice;
 
+import com.codecool.logmyphones.model.CompanyUser;
 import com.codecool.logmyphones.model.DTO.UserDTO;
 
+import java.util.Optional;
 import java.util.Set;
 
 public interface UserService {
@@ -10,4 +12,5 @@ public interface UserService {
     void addNewUser(UserDTO userDTO);
     void updateUser(Long id, UserDTO userDTO);
     void deleteUser(Long id);
+    Optional<CompanyUser> getUserByEmail(String email);
 }
