ba4ea28c4c7a6c83dd2688c24f635e150ce81d5f
oliver-nagy <olivernagy490@gmail.com>
2023-10-13 01:31:23 +0200

    refactor: refactor register and login flow

--- frontend/log-my-phones-react/src/pages/Home.js
+++ frontend/log-my-phones-react/src/pages/Home.js
@@ -1,11 +1,25 @@
 import React, {useEffect, useState} from 'react';
-import {Typography} from "@mui/material";
+import {Grid, Typography} from "@mui/material";
 import {Outlet, useNavigate} from "react-router-dom";
+import NavigationMenu from "../components/NavigationMenu";
+import {createTheme, ThemeProvider} from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Headline from "../components/Headline";
+import DisplayDashboard from "../components/DisplayDashboard";
+import LoginWindow from "../components/LoginWindow";
+import RegisterWindow from "../components/RegisterWindow";
+
+const theme = createTheme({
+    typography: {
+        fontFamily: "'Outfit', sans-serif"
+    }
+})
 
 function Home(props) {
     const token = localStorage.getItem("jsonwebtoken");
-
     const navigate = useNavigate();
+    const [showLoginWindow, setShowLoginWindow] = useState(false);
+    const [showRegisterWindow, setShowRegisterWindow] = useState(false);
 
     useEffect(() => {
         if (token) {
@@ -13,13 +27,24 @@ function Home(props) {
         }
     }, []);
 
+    function handleLoginClick() {
+        setShowLoginWindow(true);
+    }
+
+    function handleRegisterClick() {
+        setShowRegisterWindow(true);
+    }
+
     return (
-        <div>
-            <Typography variant='h1'>
-                Home Page
-            </Typography>
-            <Outlet />
-        </div>
+        <ThemeProvider theme={theme}>
+            {showLoginWindow ? <LoginWindow /> : null}
+            {showRegisterWindow ? <RegisterWindow /> : null}
+            <NavigationMenu onLogin={handleLoginClick} onRegister={handleRegisterClick}/>
+            <Box sx={{width:'1100px', mx:'auto'}}>
+                <Headline />
+                <DisplayDashboard theme={theme}/>
+            </Box>
+        </ThemeProvider>
     );
 }
 
