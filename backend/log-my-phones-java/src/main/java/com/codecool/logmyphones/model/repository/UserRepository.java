01777645f749fa9307a6c0f36788dfdb02669ba1
Kapus Tamás <kapustamas@gmail.com>
2023-10-12 16:02:23 +0200

    feat: add deleteByEmail method

--- backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/model/repository/UserRepository.java
+++ backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/model/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.codecool.logmyphones.model.repository;
 
 import com.codecool.logmyphones.model.CompanyUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<CompanyUser,Long> {
     Optional<CompanyUser> findByEmail(String email);
+    CompanyUser deleteByEmail(String email);
 }
