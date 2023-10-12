96714501481a591ed9a6452e55945f466dd7fbef
oliver-nagy <olivernagy490@gmail.com>
2023-10-12 15:28:14 +0200

    feat: implement register component

--- frontend/log-my-phones-react/src/components/RegisterWindow.js
+++ frontend/log-my-phones-react/src/components/RegisterWindow.js
@@ -3,22 +3,22 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+import {Alert, Snackbar} from "@mui/material";
+import {grey} from "@mui/material/colors";
 
 function Copyright(props) {
     return (
         <Typography variant="body2" color="text.secondary" align="center" {...props}>
             {'Copyright © '}
             <Link color="inherit" href="https://mui.com/">
-                Your Website
+                logmyphones.com
             </Link>{' '}
             {new Date().getFullYear()}
             {'.'}
@@ -31,18 +31,73 @@ function Copyright(props) {
 const defaultTheme = createTheme();
 
 export default function RegisterWindow() {
-    const handleSubmit = (event) => {
+    const AUTH_REGISTRATION_URL = "api/auth/register";
+
+    const [openPwValidationAlert, setOpenPwValidationAlert] = React.useState(false);
+    const [openRegistrationSuccess, setOpenRegistrationSuccess] = React.useState(false);
+
+    const sendRegistrationRequest = async (url, registrationFormData) => {
+        try {
+            return await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(registrationFormData)
+            });
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    const isPasswordValid = (password, confirmPassword) => {
+        return password === confirmPassword;
+    }
+
+    const handleSubmit = async (event) => {
         event.preventDefault();
         const data = new FormData(event.currentTarget);
-        console.log({
-            email: data.get('email'),
-            password: data.get('password'),
-        });
+
+        const name = data.get('companyName');
+        const email = data.get('email');
+        const password = data.get('password');
+        const confirmPassword = data.get('confirmPassword');
+
+        const registrationData = {
+            name: name,
+            email: email,
+            password: password
+        }
+
+        if (isPasswordValid(password, confirmPassword)) {
+            const response = await sendRegistrationRequest(AUTH_REGISTRATION_URL, registrationData);
+            setOpenRegistrationSuccess(true);
+
+        } else {
+            setOpenPwValidationAlert(true);
+        }
     };
+
+    const handleClosePwAlert = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpenPwValidationAlert(false);
+    };
+
+    const handleCloseRegistrationSuccess = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpenRegistrationSuccess(false);
+    };
+
 
     return (
         <ThemeProvider theme={defaultTheme}>
-            <Grid container component="main" sx={{ height: '100vh' }}>
+            <Grid container component={Paper} elevation={6} sx={{ height: '100vh', borderRadius: 4 }}>
                 <CssBaseline />
                 <Grid
                     item
@@ -56,9 +111,11 @@ export default function RegisterWindow() {
                             t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
                         backgroundSize: 'cover',
                         backgroundPosition: 'center',
+                        borderRadius: 4,
+                        height: '100%'
                     }}
                 />
-                <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                <Grid item xs={12} sm={8} md={5}>
                     <Box
                         sx={{
                             my: 8,
@@ -69,12 +126,24 @@ export default function RegisterWindow() {
                         }}
                     >
                         <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                            <LockOutlinedIcon />
+                            <PersonAddIcon />
                         </Avatar>
                         <Typography component="h1" variant="h5">
-                            Sign in
+                            Register
                         </Typography>
                         <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+                            <Typography align="center" variant="subtitle1" sx={{color: grey[600]}}>
+                                Please fill form to register.
+                            </Typography>
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="companyName"
+                                label="Company Name"
+                                name="companyName"
+                                autoFocus
+                            />
                             <TextField
                                 margin="normal"
                                 required
@@ -83,7 +152,6 @@ export default function RegisterWindow() {
                                 label="Email Address"
                                 name="email"
                                 autoComplete="email"
-                                autoFocus
                             />
                             <TextField
                                 margin="normal"
@@ -93,11 +161,15 @@ export default function RegisterWindow() {
                                 label="Password"
                                 type="password"
                                 id="password"
-                                autoComplete="current-password"
                             />
-                            <FormControlLabel
-                                control={<Checkbox value="remember" color="primary" />}
-                                label="Remember me"
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="confirmPassword"
+                                label="Confirm Password"
+                                type="password"
+                                id="confirmPassword"
                             />
                             <Button
                                 type="submit"
@@ -105,17 +177,22 @@ export default function RegisterWindow() {
                                 variant="contained"
                                 sx={{ mt: 3, mb: 2 }}
                             >
-                                Sign In
+                                Register
                             </Button>
+                            <Snackbar open={openPwValidationAlert} autoHideDuration={6000} onClose={handleClosePwAlert}>
+                                <Alert onClose={handleClosePwAlert} severity="error" sx={{ width: '100%' }}>
+                                    Passwords don't match. Please try again!
+                                </Alert>
+                            </Snackbar>
+                            <Snackbar open={openRegistrationSuccess} autoHideDuration={6000} onClose={handleCloseRegistrationSuccess}>
+                                <Alert onClose={handleCloseRegistrationSuccess} severity="success" sx={{ width: '100%' }}>
+                                    Registration successful!
+                                </Alert>
+                            </Snackbar>
                             <Grid container>
-                                <Grid item xs>
-                                    <Link href="#" variant="body2">
-                                        Forgot password?
-                                    </Link>
-                                </Grid>
                                 <Grid item>
                                     <Link href="#" variant="body2">
-                                        {"Don't have an account? Sign Up"}
+                                        {"Already have an account? Sign In"}
                                     </Link>
                                 </Grid>
                             </Grid>
