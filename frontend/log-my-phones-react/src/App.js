bc24194020f50dc303637c669e92b0aacbaecfb5
Csaba <csabi627@gmail.com>
2023-10-12 11:29:46 +0200

    feat:Implement RegisterWindow component

--- frontend/log-my-phones-react/src/App.js
+++ frontend/log-my-phones-react/src/App.js
@@ -5,14 +5,18 @@ import RootLayout from './layouts/RootLayout';
 import Dashboard from './pages/Dashboard';
 import Statistics from './pages/Statistics';
 import AddPhone from './pages/AddPhone';
+import LoginWindow from './components/LoginWindow';
+import RegisterWindow from './components/RegisterWindow';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
-      <Route path="dashboard" element={<Dashboard/>}></Route>
+      <Route path='login' element={<LoginWindow />}></Route>
+      <Route path='register' element={<RegisterWindow />}></Route>
+      <Route path="dashboard" element={<Dashboard />}></Route>
       <Route path="contacts" element={<Contacts />}></Route>
-      <Route path="statics" element={<Statistics/>}></Route>
-      <Route path="addphone" element={<AddPhone/>}></Route>
+      <Route path="statics" element={<Statistics />}></Route>
+      <Route path="addphone" element={<AddPhone />}></Route>
     </Route>
   )
 )
