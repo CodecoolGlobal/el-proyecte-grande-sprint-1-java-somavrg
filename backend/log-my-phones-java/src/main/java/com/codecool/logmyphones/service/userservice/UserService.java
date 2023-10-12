c98cdc15f047eb77e705debe9a1c4d6b71cb0af4
somavrg <varga.j.soma@gmail.com>
2023-10-12 11:35:59 +0200

    feat: fix User endpoints with ResponseEntity returns

--- backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/service/userservice/UserService.java
+++ backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/service/userservice/UserService.java
@@ -2,15 +2,17 @@ package com.codecool.logmyphones.service.userservice;
 
 import com.codecool.logmyphones.model.CompanyUser;
 import com.codecool.logmyphones.model.DTO.UserDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.util.Optional;
 import java.util.Set;
 
 public interface UserService {
-    Set<UserDTO> getAllUsers();
-    UserDTO getUserById(Long id);
-    void addNewUser(UserDTO userDTO);
-    void updateUser(Long id, UserDTO userDTO);
-    void deleteUser(Long id);
+    ResponseEntity<Set<UserDTO>> getAllUsers();
+    ResponseEntity<UserDTO> getUserById(Long id);
+    ResponseEntity<UserDTO> addNewUser(UserDTO userDTO);
+    ResponseEntity<UserDTO> updateUser(Long id, UserDTO userDTO);
+    HttpStatus deleteUser(Long id);
     Optional<CompanyUser> getUserByEmail(String email);
 }
