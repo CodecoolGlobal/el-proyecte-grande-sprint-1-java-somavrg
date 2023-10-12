28b986831e8d86b258981350850080dd98a5479c
oliver-nagy <olivernagy490@gmail.com>
2023-10-12 17:42:29 +0200

    feat: change route structure

--- frontend/log-my-phones-react/src/App.js
+++ frontend/log-my-phones-react/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
 import Contacts from './pages/Contacts';
 import RootLayout from './layouts/RootLayout';
 import Dashboard from './pages/Dashboard';
@@ -7,24 +7,26 @@ import Statistics from './pages/Statistics';
 import AddPhone from './pages/AddPhone';
 import LoginWindow from './components/LoginWindow';
 import RegisterWindow from './components/RegisterWindow';
+import Home from "./pages/Home";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
-      <Route path='login' element={<LoginWindow />}></Route>
-      <Route path='register' element={<RegisterWindow />}></Route>
-      <Route path="dashboard" element={<Dashboard />}></Route>
-      <Route path="contacts" element={<Contacts />}></Route>
-      <Route path="statics" element={<Statistics />}></Route>
-      <Route path="addphone" element={<AddPhone />}></Route>
-    </Route>
-  )
+    createRoutesFromElements(
+        [
+            <Route path="/" element={<Home/>}/>,
+            <Route path="/app" element={<RootLayout/>}>
+                <Route path="dashboard" element={<Dashboard/>}/>
+                <Route path="contacts" element={<Contacts/>}/>
+                <Route path="statics" element={<Statistics/>}/>
+                <Route path="addphone" element={<AddPhone/>}/>
+            </Route>
+        ]
+    )
 )
 
 function App() {
-  return (
-    <RouterProvider router={router} />
-  );
+    return (
+        <RouterProvider router={router}/>
+    );
 }
 
 export default App;
---
+++ frontend/log-my-phones-react/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Typography} from "@mui/material";
+
+function Home(props) {
+    return (
+        <div>
+            <Typography variant='h1'>
+                Home Page
+            </Typography>
+        </div>
+    );
+}
+
+export default Home;
