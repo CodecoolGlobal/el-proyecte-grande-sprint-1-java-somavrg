b40b25e828c6906d18b4d13bffc74e45a42447d5
oliver-nagy <olivernagy490@gmail.com>
2023-10-12 18:03:15 +0200

    feat: add token checking on home page

--- frontend/log-my-phones-react/src/pages/Home.js
+++ frontend/log-my-phones-react/src/pages/Home.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Typography} from "@mui/material";
+import {Outlet, useNavigate} from "react-router-dom";
 
 function Home(props) {
+    const hasToken = localStorage.getItem("jsonwebtoken");
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        console.log(hasToken)
+        if (hasToken) {
+            navigate("/app");
+        }
+    }, []);
+
     return (
         <div>
             <Typography variant='h1'>
                 Home Page
             </Typography>
+            <Outlet />
         </div>
     );
 }
