45ca5c3707326cd8af632afbd0dcd5e64768cc54
oliver-nagy <olivernagy490@gmail.com>
2023-12-04 16:50:05 +0100

    refactor: change layout of register window

--- frontend/log-my-phones-react/src/components/RegisterWindow.js
+++ frontend/log-my-phones-react/src/components/RegisterWindow.js
@@ -14,6 +14,17 @@ import {Alert, Snackbar} from "@mui/material";
 import {grey} from "@mui/material/colors";
 import {useNavigate} from "react-router-dom";
 
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    p: 4,
+    height: 'auto',
+    borderRadius: 4,
+    width: '1200px'
+};
+
 function Copyright(props) {
     return (
         <Typography variant="body2" color="text.secondary" align="center" {...props}>
@@ -96,127 +107,114 @@ export default function RegisterWindow({onRegister}) {
 
     return (
         <ThemeProvider theme={defaultTheme}>
-            <Box sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                position: 'absolute',
-                width: '100vw',
-                minHeight: '100vh',
-                overflow: 'hidden',
-                backgroundColor: 'rgba(0, 0, 0, 0.7)',
-                zIndex: 10,
-                paddingY: 10
-            }}>
-                <Grid container component={Paper} elevation={6} sx={{height: 'auto', borderRadius: 4, width: '1200px'}}>
-                    <CssBaseline/>
-                    <Grid
-                        item
-                        xs={false}
-                        sm={5}
-                        md={6}
+            <Grid container component={Paper} elevation={6} sx={style}>
+                <CssBaseline/>
+                <Grid
+                    item
+                    xs={false}
+                    sm={5}
+                    md={6}
+                    sx={{
+                        backgroundImage: "url('/register.jpg')",
+                        backgroundRepeat: 'no-repeat',
+                        backgroundColor: (t) =>
+                            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                        borderRadius: 4,
+                        height: 'auto'
+                    }}
+                />
+                <Grid item xs={12} sm={7} md={6}>
+                    <Box
                         sx={{
-                            backgroundImage: "url('/register.jpg')",
-                            backgroundRepeat: 'no-repeat',
-                            backgroundColor: (t) =>
-                                t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-                            backgroundSize: 'cover',
-                            backgroundPosition: 'center',
-                            borderRadius: 4,
-                            height: 'auto'
+                            my: 8,
+                            mx: 4,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            padding: '30px'
                         }}
-                    />
-                    <Grid item xs={12} sm={7} md={6}>
-                        <Box
-                            sx={{
-                                my: 8,
-                                mx: 4,
-                                display: 'flex',
-                                flexDirection: 'column',
-                                alignItems: 'center',
-                                padding: '30px'
-                            }}
-                        >
-                            <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
-                                <PersonAddIcon/>
-                            </Avatar>
-                            <Typography component="h1" variant="h5">
-                                Register
+                    >
+                        <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
+                            <PersonAddIcon/>
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Register
+                        </Typography>
+                        <Box component="form" noValidate onSubmit={handleSubmit} sx={{mt: 1}}>
+                            <Typography align="center" variant="subtitle1" sx={{color: grey[600]}}>
+                                Please fill form to register.
                             </Typography>
-                            <Box component="form" noValidate onSubmit={handleSubmit} sx={{mt: 1}}>
-                                <Typography align="center" variant="subtitle1" sx={{color: grey[600]}}>
-                                    Please fill form to register.
-                                </Typography>
-                                <TextField
-                                    margin="normal"
-                                    required
-                                    fullWidth
-                                    id="companyName"
-                                    label="Company Name"
-                                    name="companyName"
-                                    autoFocus
-                                />
-                                <TextField
-                                    margin="normal"
-                                    required
-                                    fullWidth
-                                    id="email"
-                                    label="Email Address"
-                                    name="email"
-                                    autoComplete="email"
-                                />
-                                <TextField
-                                    margin="normal"
-                                    required
-                                    fullWidth
-                                    name="password"
-                                    label="Password"
-                                    type="password"
-                                    id="password"
-                                />
-                                <TextField
-                                    margin="normal"
-                                    required
-                                    fullWidth
-                                    name="confirmPassword"
-                                    label="Confirm Password"
-                                    type="password"
-                                    id="confirmPassword"
-                                />
-                                <Button
-                                    type="submit"
-                                    fullWidth
-                                    variant="contained"
-                                    sx={{mt: 3, mb: 2}}
-                                >
-                                    Register
-                                </Button>
-                                <Snackbar open={openPwValidationAlert} autoHideDuration={6000}
-                                          onClose={handleClosePwAlert}>
-                                    <Alert onClose={handleClosePwAlert} severity="error" sx={{width: '100%'}}>
-                                        Passwords don't match. Please try again!
-                                    </Alert>
-                                </Snackbar>
-                                <Snackbar open={openRegistrationSuccess} autoHideDuration={6000}
-                                          onClose={handleCloseRegistrationSuccess}>
-                                    <Alert onClose={handleCloseRegistrationSuccess} severity="success"
-                                           sx={{width: '100%'}}>
-                                        Registration successful!
-                                    </Alert>
-                                </Snackbar>
-                                <Grid container>
-                                    <Grid item>
-                                        <Link href="#" variant="body2">
-                                            {"Already have an account? Sign In"}
-                                        </Link>
-                                    </Grid>
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="companyName"
+                                label="Company Name"
+                                name="companyName"
+                                autoFocus
+                            />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                            />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                            />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="confirmPassword"
+                                label="Confirm Password"
+                                type="password"
+                                id="confirmPassword"
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{mt: 3, mb: 2}}
+                            >
+                                Register
+                            </Button>
+                            <Snackbar open={openPwValidationAlert} autoHideDuration={6000}
+                                      onClose={handleClosePwAlert}>
+                                <Alert onClose={handleClosePwAlert} severity="error" sx={{width: '100%'}}>
+                                    Passwords don't match. Please try again!
+                                </Alert>
+                            </Snackbar>
+                            <Snackbar open={openRegistrationSuccess} autoHideDuration={6000}
+                                      onClose={handleCloseRegistrationSuccess}>
+                                <Alert onClose={handleCloseRegistrationSuccess} severity="success"
+                                       sx={{width: '100%'}}>
+                                    Registration successful!
+                                </Alert>
+                            </Snackbar>
+                            <Grid container>
+                                <Grid item>
+                                    <Link href="#" variant="body2">
+                                        {"Already have an account? Sign In"}
+                                    </Link>
                                 </Grid>
-                                <Copyright sx={{mt: 5}}/>
-                            </Box>
+                            </Grid>
+                            <Copyright sx={{mt: 5}}/>
                         </Box>
-                    </Grid>
+                    </Box>
                 </Grid>
-            </Box>
+            </Grid>
         </ThemeProvider>
     );
 }
