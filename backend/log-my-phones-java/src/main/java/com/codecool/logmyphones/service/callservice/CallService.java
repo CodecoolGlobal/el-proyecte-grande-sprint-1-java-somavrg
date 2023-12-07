28f3d1c6717365270bb5eca26116b0b1df27087f
oliver-nagy <olivernagy490@gmail.com>
2023-12-07 16:37:38 +0100

    feat: change return type of get calls method

--- backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/service/callservice/CallService.java
+++ backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/service/callservice/CallService.java
@@ -4,6 +4,7 @@ import com.codecool.logmyphones.authentication.AuthenticationResponse;
 import com.codecool.logmyphones.model.Call;
 import com.codecool.logmyphones.model.CallStatus;
 import com.codecool.logmyphones.model.DTO.CallDTO;
+import com.codecool.logmyphones.model.DTO.CallResponse;
 import com.codecool.logmyphones.model.DTO.NewCallDTO;
 import org.springframework.http.ResponseEntity;
 
@@ -11,7 +12,7 @@ import java.util.List;
 import java.util.Set;
 
 public interface CallService {
-    List<CallDTO> getCalls(String token);
+    CallResponse getCalls(String token, int pageNo, int pageSize);
     ResponseEntity<List<CallDTO>> getCallsByDispatchers(Set<Long> id);
     ResponseEntity<List<CallDTO>> getCallsByStatus(String token,CallStatus callStatus);
     CallDTO addNewCall(NewCallDTO newCallDTO);
