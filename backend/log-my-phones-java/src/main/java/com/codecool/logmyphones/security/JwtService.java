0845c5b0b55862dddc0480094f33aa1b2d6ccd85
Csege <cseloteigergely@gmail.com>
2023-12-07 10:54:32 +0100

    refactor Secret key in SwtService

--- backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/security/JwtService.java
+++ backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/security/JwtService.java
@@ -17,7 +17,7 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "abcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabc";
+    private static final String SECRET_KEY = System.getenv("SECRETKEY");
 
     public String extractUsername(String token){
         return extractClaim(token, Claims::getSubject);
