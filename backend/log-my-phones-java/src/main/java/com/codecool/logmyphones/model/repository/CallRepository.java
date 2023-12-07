28a6d5a9bca0a76d1883e0e6be2f29ab439862c6
oliver-nagy <olivernagy490@gmail.com>
2023-12-07 16:36:39 +0100

    feat: add method to retrieve calls by multiple dispatcher IDs in call repository with pagination.

--- backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/model/repository/CallRepository.java
+++ backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/model/repository/CallRepository.java
@@ -2,11 +2,15 @@ package com.codecool.logmyphones.model.repository;
 
 import com.codecool.logmyphones.model.Call;
 import com.codecool.logmyphones.model.CallStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.Set;
 
 public interface CallRepository extends JpaRepository<Call,Long> {
     Set<Call> findCallsByDispatcherId(Long dispatcherId);
+    Page<Call> findCallsByDispatcherIdIn(Collection<Long> dispatcherIds, Pageable pageable);
     Set<Call> getCallsByDispatcherIdAndCallStatus(Long dispatcherId,CallStatus callStatus);
 }
