cca27a30e39dd212eff62a7d24a19904039f9889
oliver-nagy <olivernagy490@gmail.com>
2023-12-13 14:38:11 +0100

    feat(CallRepository): add filter by call direction jpql method

--- backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/model/repository/CallRepository.java
+++ backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/model/repository/CallRepository.java
@@ -1,6 +1,7 @@
 package com.codecool.logmyphones.model.repository;
 
 import com.codecool.logmyphones.model.Call;
+import com.codecool.logmyphones.model.CallDirection;
 import com.codecool.logmyphones.model.CallStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,5 +13,8 @@ import java.util.Set;
 public interface CallRepository extends JpaRepository<Call,Long> {
     Set<Call> findCallsByDispatcherId(Long dispatcherId);
     Page<Call> findCallsByDispatcherIdIn(Collection<Long> dispatcherIds, Pageable pageable);
+    Page<Call> findCallsByDispatcherIdInAndCallDirection(Collection<Long> dispatcherIds,
+                                                         CallDirection callDirection,
+                                                         Pageable pageable);
     Set<Call> getCallsByDispatcherIdAndCallStatus(Long dispatcherId,CallStatus callStatus);
 }
