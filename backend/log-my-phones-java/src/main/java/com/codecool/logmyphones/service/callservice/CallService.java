44ba1319dfb62e021332fe223d9f860892b086fa
oliver-nagy <olivernagy490@gmail.com>
2023-12-13 14:37:25 +0100

    feat(CallService): add callDirection parameter

--- backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/service/callservice/CallService.java
+++ backend/log-my-phones-java/src/main/java/com/codecool/logmyphones/service/callservice/CallService.java
@@ -2,6 +2,7 @@ package com.codecool.logmyphones.service.callservice;
 
 import com.codecool.logmyphones.authentication.AuthenticationResponse;
 import com.codecool.logmyphones.model.Call;
+import com.codecool.logmyphones.model.CallDirection;
 import com.codecool.logmyphones.model.CallStatus;
 import com.codecool.logmyphones.model.DTO.CallDTO;
 import com.codecool.logmyphones.model.DTO.CallResponse;
@@ -12,7 +13,7 @@ import java.util.List;
 import java.util.Set;
 
 public interface CallService {
-    CallResponse getCalls(String token, int pageNo, int pageSize);
+    CallResponse getCalls(String token, int pageNo, int pageSize, CallDirection callDirection);
     ResponseEntity<List<CallDTO>> getCallsByDispatchers(Set<Long> id);
     ResponseEntity<List<CallDTO>> getCallsByStatus(String token,CallStatus callStatus);
     CallDTO addNewCall(NewCallDTO newCallDTO);
